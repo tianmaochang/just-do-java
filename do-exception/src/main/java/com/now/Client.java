fa006741e79e4649b11ef4e466023c99f94cc1ee
Now <5643182+tianmaochang@users.noreply.github.com>
2019-10-15 10:00:05 +0800

    Update Client.java

--- do-exception/src/main/java/com/now/Client.java
+++ do-exception/src/main/java/com/now/Client.java
@@ -2,7 +2,9 @@ package com.now;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+/**
+* 测试类
+*/
 public class Client {
     public static void main(String[] args) {
         try {
